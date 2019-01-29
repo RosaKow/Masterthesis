23c7392554e2634de8a99c30bc84310a01bb4404
Leander - Flow (LJLL) <leander.lacroix@ljll.math.upmc.fr>
2019-01-29 17:17:41 +0100

    Added image sampling function along with a Jupyter notebook example.

--- Sampling.py
+++ Sampling.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import matplotlib.image
+import UsefulFunctions
 
 import matplotlib.pyplot as plt
 
@@ -50,6 +51,7 @@ def sampleFromGreyscale(filename, threshold=1e-2, centered=False, normaliseWeigh
 
             x[count, 0] = i/img.shape[0]
             x[count, 1] = j/img.shape[1]
+            #alpha[count] = img[img.shape[0] - i - 1, j]
             alpha[count] = img[img.shape[0] - i - 1, j]
 
             count = count + 1
@@ -61,6 +63,33 @@ def sampleFromGreyscale(filename, threshold=1e-2, centered=False, normaliseWeigh
         alpha = alpha/np.sum(alpha)
     
     return torch.tensor(x, dtype=torch.get_default_dtype()), torch.tensor(alpha, dtype=torch.get_default_dtype())
+
+
+def gaussianKernel(x, sigma):
+    return 1/np.sqrt(2*np.pi)/sigma*np.exp(-0.5*x**2/sigma**2)
+
+
+def kernelSmoother(x, data, kernel=gaussianKernel, sigma=0.1):
+    """Apply a kernel smoother on the values at x."""
+    y = torch.zeros(x.shape[0])
+    for i in range(0, x.shape[0]):
+        y[i] = np.sum(kernel(np.linalg.norm((x[i,:].expand_as(data[0]) - data[0]).numpy(), axis=1), sigma)*data[1].numpy())
+
+    return y
+
+
+def sampleImageFromPoints(data, res, kernel=gaussianKernel, sigma=0.1, normalize=True, aabb=None):
+    """Sample an image from a list of smoothened points."""
+    if(aabb is None):
+        aabb = UsefulFunctions.AABB()
+        aabb.buildFromPoints(data[0])
+    x, y = torch.meshgrid([torch.arange(aabb.minx-sigma, aabb.maxx+sigma, step=(aabb.maxx-aabb.minx+2.*sigma)/res[0]), torch.arange(aabb.miny-sigma, aabb.maxy+sigma, step=(aabb.maxy-aabb.miny+2.*sigma)/res[1])])
+    vec = UsefulFunctions.grid2vec(x, y)
+    pixels = kernelSmoother(vec, data, sigma=sigma)
+    if(normalize):
+        pixels = pixels/torch.sum(pixels)
+
+    return pixels.view(res[0], res[1])
 
 
 def sampleFromShape2D(nbPts, shape, K):
