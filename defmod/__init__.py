fa6a434113c80aa3905bf653c7b71b48c9b2fc7b
Leander - Flow (LJLL) <leander.lacroix@ljll.math.upmc.fr>
2019-02-05 11:21:06 +0100

    Forgot to add __init__.py for the defmod module.

---
+++ defmod/__init__.py
@@ -0,0 +1,9 @@
+from . import usefulfunctions
+from . import sampling
+from . import shooting
+from . import hamiltonian
+from . import deformationmodules
+from . import models
+
+__version__ = "0.0.1"
+
